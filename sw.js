f7463e7e4a447ab2b597108b4e2539d2d346d9a1
JIAJUN <epwujiajun@icloud.com>
2026-06-06 23:02:45 +0800

    Deploy to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'shrimp-mode-v1';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
